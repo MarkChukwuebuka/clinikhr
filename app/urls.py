6e55fae802fc182a0669b8321001575821ba958a
Mark Okonedah <markedah9652@gmail.com>
2026-04-14 23:12:23 +0100

    update

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from .views import home, services, about, contact, who_we_serve, resources, how_we_work, bfri_assessment
+from .views import home
 
 urlpatterns = [
     path('', home, name='home'),
